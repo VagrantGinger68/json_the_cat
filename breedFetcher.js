065c3e8ba24e4bba99bcfd79050c2222bc044b7c
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-08-31 15:05:12 -0400

    refactored and added tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,25 @@
 const request = require("request");
 
-const breedName = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    if (error) {
+      callback(error,null);
+      process.exit();
+    }
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
-  if (response.statusCode !== 200) {
-    console.log("Request Failed");
-    process.exit();
-  }
+    if (response.statusCode !== 200) {
+      callback("Request Failed", null);
+      process.exit();
+    }
+  
+    const data = JSON.parse(body);
 
-  const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback("Breed not found", null);
+      process.exit();
+    }
+    callback(null, data[0].description);
+  });
+};
 
-  if (data.length === 0) {
-    console.log("Breed not found");
-    process.exit();
-  }
-
-  console.log(data[0].description);
-});
+module.exports = { fetchBreedDescription };
